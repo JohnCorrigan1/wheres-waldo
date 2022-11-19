d40f611626f51b1ddc520541e6eb42d00908a19d
John Corrigan <jcorrigan322@gmail.com>
2022-11-19 18:34:20 -0500

    correct messages

---
+++ components/GameHeader.tsx
@@ -0,0 +1,10 @@
+const GameHeader: React.FC = () => {
+    return (
+        <div>
+            <h1 className="text-4xl font-semibold">Mario</h1>
+        </div>
+    
+        )
+}
+
+export default GameHeader;
