82c6f530e81e026ec087f06e18dade340d09929b
John Corrigan <jcorrigan322@gmail.com>
2022-11-22 21:30:01 -0500

    retrieve data from firestore on leaderboard page

--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -1,9 +1,28 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { toast } from "react-hot-toast";
-
+import { useEffect } from "react";
+import { collection, getDocs, query } from "firebase/firestore";
+import { db } from "../lib/firebase";
 const Leaderboard: NextPage = () => {
+
+  //retrieve data from firestore
+  const leaderBoardData: {name: string, time: number}[] = [];
+
+    useEffect(() => {
+        const getLeaderboard = async () => {
+            const q = query(collection(db, "leaderboard"));
+            const querySnapshot = await getDocs(q);
+            querySnapshot.forEach((doc) => {
+                leaderBoardData.push(doc.data() as {name: string, time: number});
+                console.log(doc.data())
+            });
+        }
+        getLeaderboard();
+    }, []);
+    
+
+
     return (
         <div className="leaderboard bg-neutral-800 text-zinc-100">
         <h1>Leaderboard</h1>
